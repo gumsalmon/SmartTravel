ebde52b85e7b2de158d1069c583caafa4ff34d66
huy <ngoduchuy17082005@gmail.com>
2026-03-31 15:21:22 +0700

    Update Luxury UI and fix Android HorizontalTextAlignment crash

---
+++ HeriStep.Client/Views/ProfilePage.xaml.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace HeriStep.Client.Views
+{
+    public partial class ProfilePage : ContentPage
+    {
+        public ProfilePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
